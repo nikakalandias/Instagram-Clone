d815b19b50b033669141c2f9052b814ce91db99f
nika <nikakalandias@gmail.com>
2021-11-01 19:22:43 +0300

    Added; Firebase Login

--- Instagram-Clone/ViewController.swift
+++ Instagram-Clone/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
@@ -23,6 +24,32 @@ class ViewController: UIViewController {
     }
     
     @IBAction func signupButton(_ sender: Any) {
+        
+        if emailText.text != "" && passwordText.text != "" {
+            Auth.auth().createUser(withEmail: emailText.text!, password: passwordText.text!) { authdata, error in
+                if error != nil {
+                    self.Alert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error!")
+                    
+                }else {
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
+            
+        }else {
+            Alert(titleInput: "Error!", messageInput: "Please enter your e-mail or password!")
+        }
+        
+
+        
+    }
+    
+    func Alert(titleInput:String, messageInput:String) {
+        
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+        
     }
 }
 
